f7d4f113624bea1ebd0fce3dff2477f44cf7d8ad
Ayush Mishra <ayushm185@gmail.com>
2023-11-24 11:13:55 +0530

    adding css

--- script.js
+++ script.js
@@ -8,35 +8,48 @@ async function gettopheadling(){
     try{
         const response=await fetch(pure_url)
         const data=await response.json()
-        console.log(data);
-            document.getElementById('right-top').innerHTML+=`<div class="topchild1"><img src="${data.articles[0].urlToImage}"><a href=${data.articles[0].url}>${data.articles[0].title}</a></div>`
-        // document.getElementById('right-top').innerHTML+=`<div class="topchild1"><img src="${data.articles[0].urlToImage}"><a href=${data.articles[0].url}>${data.articles[0].title}</a></div>`
-        // document.getElementById('top').innerHTML+=`<div class="parent"><img src="${data.articles[1].urlToImage}"><p>${data.articles[1].description}</p><a href=${data.articles[1].url}>Go to link</a></div>`
-        // data.articles.forEach(element => {
-        //     document.getElementById('left-top').innerHTML+=`<div class="topchild2"><img src="${element.urlToImage}"> <a href="${element.url}">${element.title}</a></div>`
-        // });
-        // // document.getElementById('left-top').innerHTML+=`<div class="topchild2"><img src="${data.articles[1].urlToImage}"> <a href="${data.articles[1].url}">${data.articles[1].title}</a></div>`
-        // for(let index=6;index<=8;index++)
-        // {
-        //     document.getElementById('bottom-right-top').innerHTML+=`<div class="right-topchild1"><img src="${data.articles[index].urlToImage}"><a href=${data.articles[index].url}>${data.articles[index].title}</a></div>`
-        // }
-        for(let index=1;index<=4;index++)
-        {
-           document.getElementById('left-top').innerHTML+=`<div class="topchild2"><img src="${data.articles[index].urlToImage}"> <a href="${data.articles[index].url}">${data.articles[index].title}</a></div>`   
-        }
-        for(let index=5;index<=9;index++)
-        {
-           document.getElementById('middle').innerHTML+=`<div class="middlechild"><img src="${data.articles[index].urlToImage}"> <a href="${data.articles[index].url}">${data.articles[index].title}</a></div>`   
-        }
-        for(let index=10;index<=11;index++)
-        {
-           document.getElementById('bottom').innerHTML+=`<div class="bottomchild"><img src="${data.articles[index].urlToImage}"> <a href="${data.articles[index].url}">${data.articles[index].title}</a></div>`   
-        }
-        for(let index=12;index<=19;index++)
-        {
-           document.getElementById('recent').innerHTML+=`
-           <div class="recentchild"><img src="${data.articles[index].urlToImage}"> <a href="${data.articles[index].url}">${"Click To Read Articles--"}</a></div>` 
-        }
+        const articarray=data.articles
+       // console.log(articarray);
+        const result=articarray.filter(function(data){
+             if(data.urlToImage!=null)
+             {
+                return data
+             }
+        })
+        let index=0;
+        result.forEach(function(item){
+            if(index<1)
+            {
+                document.getElementById('right-top').innerHTML+=`<div class="topchild1"><img src="${result[0].urlToImage}"><a href=${result[0].url}>${result[0].title}</a></div>`
+            }
+            else
+            {
+                if(index<=4)
+                {
+                    document.getElementById('left-top').innerHTML+=`<div class="topchild2"><img src="${result[index].urlToImage}"> <a href="${result[index].url}">${result[index].title}</a></div>`   
+                }
+                else
+                {
+                    if(index<=9)
+                    {
+                        document.getElementById('middle').innerHTML+=`<div class="middlechild"><img src="${result[index].urlToImage}"> <a href="${result[index].url}">${result[index].title}</a></div>`   
+                    }
+                    else
+                    {
+                        if(index<=11)
+                        {
+                            document.getElementById('bottom').innerHTML+=`<div class="bottomchild"><img src="${result[index].urlToImage}"> <a href="${result[index].url}">${result[index].title}</a></div>` 
+                        }
+                        else
+                        {
+                            document.getElementById('recent').innerHTML+=`
+                            <div class="recentchild item"><img src="${result[index].urlToImage}"> <a href="${result[index].url}">${"Click To Read Articles--"}</a></div>` 
+                        }
+                    }
+                }
+            }
+            index++
+        });
     } catch(error){
         console.log(error);
     }
